c5e6df1eab50f39f08d5b6d471832d0ea5aa6cb4
Daniel Bell <Dbell3592@conestogac.on.ca>
2018-10-11 17:32:10 -0400

    i think igot it

--- Game.js
+++ Game.js
@@ -3,6 +3,24 @@ const GameState = Object.freeze({
     STICK:  Symbol("stick"),
     PLAY: Symbol("play"),
     QUESTION1: Symbol("dogs"),
+    QUESTION2: Symbol("guitar"),
+    QUESTION3: Symbol("xbox"),
+    QUESTION4: Symbol("yes"),
+    QUESTION5: Symbol("dogs"),
+    QUESTION6: Symbol("dogs"),
+    QUESTION7: Symbol("dogs"),
+    QUESTION8: Symbol("dogs"),
+    QUESTION9: Symbol("dogs"),
+    QUESTION10: Symbol("dogs"),
+    QUESTION11: Symbol("dogs"),
+    QUESTION11: Symbol("dogs"),
+    QUESTION12: Symbol("dogs"),
+    QUESTION13: Symbol("dogs"),
+    QUESTION14: Symbol("dogs"),
+    QUESTION15: Symbol("dogs"),
+    QUESTION16: Symbol("dogs"),
+    QUESTION17: Symbol("dogs"),
+   
 });
 
 export default class Game{
@@ -31,7 +49,7 @@ export default class Game{
                 if(sInput.toLowerCase().match("yes")){
                     sReply = "your my best friend now lets begin! do you like dogs or cats? ";
                 }else{
-                    sReply = "i get it this is very hard for youbut i need toknow this vitalinfo now..cat or dogs?";
+                    sReply = "i get it this is very hard for you but i need to know this vital info now..cat or dogs?";
                     this.stateCur = GameState.QUESTION1;
                 }
                 break;
@@ -41,7 +59,16 @@ export default class Game{
                     sReply = "well id hope so dogs are the coolest like how many animals do you know that have 4 legs? not many.. do you play guitar? ";
                 }else{
                     sReply = "well i mean....nobodies perfect..maybe just humor me and type dogs";
-                    this.stateCur = GameState.QUESTION1;
+                    this.stateCur = GameState.QUESTION2;
+                }
+                break;
+
+             case GameState.QUESTION2:
+                if(sInput.toLowerCase().match("yes")){
+                    sReply = "good on you its the best instrument really.. in my humble opinion ";
+                }else{
+                    sReply = "well i mean....nobodies perfect..maybe just humor me and type yes";
+                    this.stateCur = GameState.QUESTION2;
                 }
                 break;
     
